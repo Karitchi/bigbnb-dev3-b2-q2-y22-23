941032ef98397f799910dfcf0624401d64b4ff0e
Karitchi <karichikarichikarichi@gmail.com>
2023-05-07 19:46:23 +0200

    suppression package bootstrap

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/variables.scss'
 
 const app = createApp(App);
