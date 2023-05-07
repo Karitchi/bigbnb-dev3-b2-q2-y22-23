93e2b2d8cfbab87c747253eb0ed01f2e7ed5c511
Karitchi <karichikarichikarichi@gmail.com>
2023-05-07 19:22:57 +0200

    ajout de la galerie photos dans la page de détails des hotels

---
+++ front/src/components/PhotoGalleryHotel.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container-fluid p-5">
+        <div class="row p-1 bg-light  rounded border m-0 ">
+            <div class="col p-1 big-col">
+                <img src="https://images.unsplash.com/photo-1582719508461-905c673771fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80" class="img-fluid rounded big" alt="">
+            </div>
+
+            <div class="col p-0">
+                <div class="row row-cols-2 m-0">
+                    <div class="col p-1 little-col">
+                        <img src="https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80" class="img-fluid rounded little" alt="">
+                    </div>
+
+                    <div class="col p-1 little-col">
+                        <img src="https://plus.unsplash.com/premium_photo-1678286769630-4ea3d6878dfb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="img-fluid rounded little" alt="">
+                    </div>
+
+                    <div class="col p-1 little-col">
+                        <img src="https://images.unsplash.com/photo-1522798514-97ceb8c4f1c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" class="img-fluid rounded little" alt="">
+                    </div>
+
+                    <div class="col p-1 little-col ">
+                        <img src="https://images.unsplash.com/photo-1679678690998-88c8711cbe5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" class="img-fluid rounded little" alt="">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.big-col {
+    height: 30vw;
+}
+
+.little-col {
+    height: 15vw;
+}
+
+.big {
+    width: 100%;
+    height: 100%;
+    object-fit: cover
+}
+
+.little {
+    width: 100%;
+    height: 100%;
+    object-fit: cover
+}
+</style>
---
+++ front/src/views/HotelDetails.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import PhotoGalleryHotel from '@/components/PhotoGalleryHotel.vue';
+
+</script>
+
+<template>
+    <PhotoGalleryHotel />
+</template>
+
+<style scoped></style>
+
+
