60a4a2ff78ea66e0d407a3cdbe11a1297d85277a
Karitchi <karichikarichikarichi@gmail.com>
2023-05-08 15:11:10 +0200

    amélioration de la home page
    La page récupère maintenant certaines données de la db et les ajoutes dynamiquement au cartes des hotels.
    La syntaxe de vue à été changé pour utiliser l'api composition

--- front/src/components/CardHotel.vue
+++ front/src/components/CardHotel.vue
@@ -1,15 +1,23 @@
+<script setup lang="ts">
+defineProps([
+  "name",
+  "description",
+  "hotel_owner",
+])
+</script>
+
 <template>
-    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 p-3" id="hotel-card">
-        <div class="card m-auto">
-            <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h3 class="cart-title">{{ this.hotel.name }}</h3>
-              <p class="card-text"> <i>{{ this.hotel.description }}</i></p>
-                <a href="#" class="btn btn-outline-primary" style="margin-bottom: 5px">Plus d'informations</a>
-              <p class="card-footer">By <i>{{ this.hotelOwner.company }}</i></p>
-            </div>
-        </div>
+  <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 p-3" id="hotel-card">
+    <div class="card m-auto">
+      <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h3 class="cart-title">{{ name }}</h3>
+        <p class="card-text"> <i>{{ description }}</i></p>
+        <a href="#" class="btn btn-outline-primary" style="margin-bottom: 5px">Plus d'informations</a>
+        <p class="card-footer">By <i>{{ hotel_owner }}</i></p>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
@@ -17,28 +25,3 @@
   display: inline-block;
 }
 </style>
-<script>
-import axios from "axios";
-
-
-export default {
-  name: 'Card',
-  props: ['hotel'],
-  data() {
-    return {
-      hotelOwner: {},
-    }
-  },
-
-  methods: {
-    setHotelOwner() {
-      axios.get(`${this.$api}hotel_owners/${this.hotel.hotel_owner}`)
-      .then(response => this.hotelOwner = response.data);
-    }
-  },
-
-  mounted() {
-    this.setHotelOwner();
-  }
-}
-</script>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,42 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import CardHotel from '@/components/CardHotel.vue'
+
+const hotels = ref([])
+
+fetch('http://127.0.0.1:8000/hotels/')
+  .then(response => response.json())
+  .then(response => hotels.value = response)
+</script>
+
+
 <template>
-  <div class="container-fluid p-5">
-    <div v-for="hotel in this.hotels" class="row m-0">
-      <card-hotel :hotel="hotel" />
-    </div>
-  </div>
+  <CardHotel v-for="hotel in hotels" :key="hotel.id" :name="hotel.name" :description="hotel.description" :hotel_owner="hotel.hotel_owner" />
 </template>
 
-<style scoped>
-  @import '../assets/variables.scss'
-</style>
+<style scoped></style>
 
 
-<script>
-import CardHotel from '../components/CardHotel.vue'
-import axios from "axios";
-
-export default {
-  name: 'App',
-  components: {
-        CardHotel,
-  },
-
-  data() {
-    return {
-      hotels: []
-    }
-  },
-
-  methods: {
-    fetchAllHotels() {
-      axios.get(`${this.$api}hotels/`)
-      .then(response => this.hotels = response.data)
-    }
-  },
-
-  mounted() {
-    this.fetchAllHotels();
-  }
-
-}
-</script>
