4cbfb9f5f45285c9e625d3c9ad0d077f5f7cd13c
Coco <co.koninckx@gmail.com>
2023-05-10 02:23:08 +0200

    login data

--- back/big_b_n_b/settings.py
+++ back/big_b_n_b/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'big_bnb',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'coco',
+        'PASSWORD': 'sql',
         'HOST': 'localhost',
         'PORT': '3306',
     }
