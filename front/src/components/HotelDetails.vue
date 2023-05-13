ddd8cbb58f9e615220084d8d0bf7aef47318d57a
Karitchi <karichikarichikarichi@gmail.com>
2023-05-13 19:26:11 +0200

    rework du design + ajout de données pour la page details

--- front/src/components/HotelDetails.vue
+++ front/src/components/HotelDetails.vue
@@ -1,82 +1,88 @@
 <script setup lang="ts">
-defineProps([
-    "name",
-    "hotel_owner",
-    "image",
-    "description",
-    "city",
-    "rooms",
-    "price",
-])
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router';
 
+const route = useRoute()
+
+const hotel = ref(null)
+const city = ref(null)
+const hotel_owner = ref(null)
+
+const isLoaded = ref(false)
+
+onMounted(async () => {
+    let response
+
+    response = await fetch(`http://127.0.0.1:8000/hotels/${route.params.id}`)
+    hotel.value = await response.json()
+
+    response = await fetch(`http://127.0.0.1:8000/cities/${hotel.value.city}`)
+    city.value = await response.json()
+
+    response = await fetch(`http://127.0.0.1:8000/hotel_owners/${hotel.value.hotel_owner}`)
+    hotel_owner.value = await response.json()
+
+    console.log(hotel.value)
+    console.log(city.value)
+    console.log(hotel_owner.value)
+
+    isLoaded.value = true
+})
 </script>
 
 <template>
-    <div class="container-fluid p-5">
-        <div class="container-fluid">
-            <h1>{{ name }}</h1>
-            <hr class="hr" />
+    <div v-if="!isLoaded" class="text-center">
+        <div class="spinner-border" role="status"></div>
+    </div>
 
-            <div class="row">
-                <div class="col">
+    <div v-else class="container-fluid p-5">
 
 
-                    <div class="row">
-                        <div class="col">
-                            <div class="card bg-light">
-                                <div class="card-body">
-                                    <div class="card-title">
-                                        <h5>Hotelier: {{ hotel_owner }}</h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <h1 class="h1">{{ hotel.name }}</h1>
+        <h3>{{ hotel.description }}</h3>
+        <hr>
 
-                        <div class="col">
-                            <div class="card bg-light">
-                                <div class="card-body">
-                                    <div class="card-title">
-                                        <h5>Dans la ville de {{ city }}</h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="col">
-                            <div class="card bg-light">
-                                <div class="card-body">
-                                    <div class="card-title">
-                                        <h5>Il reste {{ rooms }} chambre</h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <hr class="hr" />
-                    <div class="row">
-                        <div class="col">
-                            <div class="card bg-light">
-                                <div class="card-body">
-                                    <div class="card-title">
-                                        <h5> {{ description }}</h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
+        <div class="row">
+            <div class="col">
+                <div class="card text-center bg-secondary-subtle">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ hotel.rooms }} rooms left</h5>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card bg-light">
-                        <div class="card-body">
-                            <div class="card-title">
-                                <h5> {{ price }}</h5>
-                            </div>
-                        </div>
+            </div>
+            <div class="col">
+                <div class="card text-center bg-secondary-subtle">
+                    <div class="card-body">
+                        <h5 class="card-title">Located in {{ city.name }}, {{ city.country }}</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-center bg-secondary-subtle">
+                    <div class="card-body">
+                        <h5 class="card-title">For {{ hotel.price }} dollars</h5>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- <h1>{{ price }}</h1>
-            <h1>{{ image }}</h1> -->
+
+        <hr>
+        <div class="row">
+            <div class="col">
+                <div class="card text-center bg-secondary-subtle">
+                    <div class="card-body">
+                        <h5 class="card-title">Proposed by {{ hotel_owner.name }} {{ hotel_owner.lastname }} from {{ hotel_owner.company }}</h5>
+                        <p class="card-text">Contact: {{ hotel_owner.mail }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- <p>{{ city.location_x }}</p>
+        <p>{{ city.location_y }}</p> -->
+
     </div>
 </template>
--- front/src/views/HotelDetails.vue
+++ front/src/views/HotelDetails.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRoute } from 'vue-router';
 import PhotoGalleryHotel from '@/components/PhotoGalleryHotel.vue';
 import HotelDetails from '@/components/HotelDetails.vue';
 
-const hotel = ref([])
-const route = useRoute()
 
-fetch(`http://127.0.0.1:8000/hotels/${route.params.id}`)
-    .then(response => response.json())
-    .then(response => hotel.value = response)
+
 </script>
 
 <template>
     <PhotoGalleryHotel />
-    <HotelDetails :name="hotel.name" :hotel_owner="hotel.hotel_owner" :image="hotel.image" :description="hotel.description" :city="hotel.city" :rooms="hotel.rooms" :price="hotel.price" />
+    <HotelDetails />
 </template>
 
 
