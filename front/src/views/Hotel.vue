27b3906b257162660bb1fc7fa9e49b18f4add7bb
Karitchi <karichikarichikarichi@gmail.com>
2023-05-23 16:56:35 +0200

    ajout du bouton retour

--- front/src/views/Hotel.vue
+++ front/src/views/Hotel.vue
@@ -3,12 +3,13 @@ import Images from '@/components/Hotel/Images.vue';
 import Informations from '@/components/Hotel/Informations.vue';
 import ReviewForm from '@/components/Hotel/ReviewForm.vue'
 import ReviewsList from '@/components/Hotel/ReviewsList.vue'
-
+import BackButton from '@/components/Hotel/BackButton.vue'
 
 </script>
 
 <template>
     <div class="container ps-5 pe-5">
+        <BackButton />
         <Images />
         <Informations />
         <ReviewForm />
