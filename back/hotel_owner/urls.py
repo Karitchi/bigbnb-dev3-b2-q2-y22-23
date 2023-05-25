fe54a04c915a06f31fae2b59d461c5b785398048
MachiganMC <xnagos@gmail.com>
2023-05-25 21:17:29 +0200

    fix(hotel-info): Erreur 404 infos hôteliers
    Lorsque la page (côté front) `/hotels/<id>` était appelé, celle-ci
    faisait une requête à l'API pour obtenir les informations de son
    hôtelier celle-ci retournait toujours un '404', car la route était mal
    configurée côté backend.

--- back/hotel_owner/urls.py
+++ back/hotel_owner/urls.py
@@ -5,5 +5,5 @@ from hotel_owner import views
 
 urlpatterns = [
     path('', views.all_hotel_owners),
-    path('<int:hotel_owner_id>', views.hotel_owner_detail)
+    path('<int:hotel_owner_id>/', views.hotel_owner_detail)
 ]
