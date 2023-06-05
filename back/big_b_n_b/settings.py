19a4e1b40501814edff334570f6c8eaa95d4eb89
Maxime Renguet <m.renguet@students.ephec.be>
2023-06-05 03:20:31 +0200

    modification setting.py

--- back/big_b_n_b/settings.py
+++ back/big_b_n_b/settings.py
@@ -133,8 +133,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'big_bnb',
-        'USER': 'root',
-        'PASSWORD': '1234',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',
         'PORT': '3306',
     }
